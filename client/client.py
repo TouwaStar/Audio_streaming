05ce5ec4d7af8ffe2424654463e283c1ed989bbc
TouwaStar <silaczewski.mateusz@gmail.com>
2019-05-21 00:04:04 +0200

    minor client refactoring

--- client/client.py
+++ client/client.py
@@ -15,36 +15,61 @@ def unpack_message(message):
     pat = re.compile(b'PRE(.*?)SUF')
     return pat.findall(message)[0]
 
+class Client():
+    def __init__(self):
+        self.data = []
+        self.data_message_size = 0
+        self.sampling_rate = 0
+        self._stream = None
+
+    def _retrieve_message(self, socket, message, size=1024):
+        socket.send(b'GIVE_'+message)
+        message = socket.recv(size)
+        socket.send(b'GOT_'+message)
+        return message
+
+    def get_sampling(self, socket):
+        message = self._retrieve_message(socket, "SAMPLING_RATE")
+        self.sampling_rate = int(unpack_message(message)[0])
+
+    def get_data_message_sizesize(self, socket):
+        message = self._retrieve_message(socket, "SIZE")
+        self.data_message_size = int(unpack_message(message)[0])
+
+    def retrieve_audio_data(self, socket):
+        message = socket.recv(self.data_message_size)
+        self.data.append(message)
+        if b'EOM' in message:
+            return False
+        return True
+    
+    def initialize_audio_stream(self):
+        self.stream = p.open(format=pyaudio.paFloat32,
+                        channels=,
+                        rate=self.sampling_rate,
+                        output=True)
+
+    def play_streamed_data(self, audio_frame):
+
+
+
 
 def main():
     data = []
     message_size_b = b''
     sampling_rate = 0
     
+    client = Client()
+
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((HOST,PORT))
 
-            message = s.recv(1024)
-            print(message)
-            if(b'SAMPLING' in message):
-                s.send(b"GIVE_SAMPLING_RATE")
-                message = s.recv(1024)
-                print(message)
-                sampling_rate = int(unpack_message(message))
-                s.send(b"GOT_SAMPLING_RATE")
-            
-            s.send(b"GIVE_SIZE")
-            message_size_b = s.recv(1024)
-            starttime = time.time()
-            s.send(b"GOT_SIZE")
-            message_size = unpack_message(message_size_b)[0]
-            message_size_unpacked = int(message_size)
-            while(True):
-                message = s.recv(message_size_unpacked)
-                if (b'EOM' in message):
-                    break
-                data.append(message)
+            client.get_sampling(s)
+            client.get_data_message_sizesize(s)
+            client.initialize_audio_stream
+            while client.retrieve_audio_data(s):
+                play_streamed_data(frame)
                 
     except ConnectionResetError:
         pass
@@ -57,7 +82,6 @@ def main():
         #for message in data:
         #    print(pat.findall(message))
 
-        print(time.time() - starttime)
 
         #p = pyaudio.PyAudio()
         '''
